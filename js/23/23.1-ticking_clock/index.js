e7b9276a5e16da1028539920291bb4f57f71b3a4
Netanel Mazuz <netazuz@gmail.com>
2021-02-24 19:13:54 +0200

    submit javascript 23

---
+++ js/23/23.1-ticking_clock/index.js
@@ -0,0 +1,33 @@
+const deg = 6;
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+
+function Clock(){
+
+  //Save the times in variables
+  let today = new Date();
+  let hours = today.getHours();
+  let hh = today.getHours()*30;
+  let minutes = today.getMinutes();
+  let mm = today.getMinutes()*deg;
+  let seconds = today.getSeconds();
+  let ss = today.getSeconds()*deg;
+
+  // Analog
+  hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+  
+  // digital
+  //Set the AM or PM time
+  meridiem = hours >= 12 ? " PM" : " AM";
+  
+  //Put 0 in front of single digit minutes and seconds
+  minutes = minutes<10 ? "0" + minutes : minutes;
+  seconds = seconds<10 ? "0" + seconds : seconds;
+
+  document.getElementById("digital").innerHTML = (hours + ":" + minutes + ":" + seconds + meridiem);
+}
+
+setInterval('Clock()', 1000);
