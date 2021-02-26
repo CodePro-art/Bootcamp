0ab7a9390c567e22c1532b0dbfc72423da8a903a
Netanel Mazuz <netazuz@gmail.com>
2021-02-26 19:00:38 +0200

    submit

--- projects/JS/6/6.3-Organoze_Strings/index.js
+++ projects/JS/6/6.3-Organoze_Strings/index.js
@@ -8,16 +8,6 @@
 // a = "abcdefghijklmnopqrstuvwxyz"
 // longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 
-const longest = (str1,str2) => {
-  let str ='';
-  str1 = str1.toLowerCase();
-  str2 = str1.toLowerCase();
-  console.log(str1);
-  for (let i = 97; i < 123 ;i++)
-    str +=  include(str1,str2,i);
-  return str;
-};
-
-const include = (a, b, i) => a.includes(String.fromCharCode(i)) || b.includes(String.fromCharCode(i)) ? String.fromCharCode(i) : "";
+const longest = (a,b) => [...new Set(a+b)].sort().join("");
 
 console.log(longest("AsUab","ud"));
