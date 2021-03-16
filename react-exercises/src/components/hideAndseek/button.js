6be6497cd48819147c180c5b12052a42bb670bfb
Netanel Mazuz <netazuz@gmail.com>
2021-03-16 14:07:34 +0200

    update

--- react-exercises/src/components/hideAndseek/button.js
+++ react-exercises/src/components/hideAndseek/button.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function button(props) {
   return (
     <div>
-      <button onClick={() => props.toggle(!props.visible)}>Hide/Show</button>
+      <button onClick={() => props.toggle()}>Hide/Show</button>
     </div>
   )
 }
