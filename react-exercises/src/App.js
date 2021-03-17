bc4ee956b23404e457299806787894be8181b362
Netanel Mazuz <netazuz@gmail.com>
2021-03-17 17:18:37 +0200

    update

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,16 +1,16 @@
-import Hello from './components/ex1-hello/hello'
-import Display from './components/ex2-basic/Basic';
-import Boxes from './components/boxes/box1';
-import Quiz from './components/quiz/quiz';
-import Buttons from './components/buttons/buttons';
-import Cards from './components/cards/cards';
-import ClassCards from './components/cards.class.component/cards';
-import Increment from './components/increment/increment';
-import HideAndSeek from './components/hideAndseek/hideAndseek';
-import FavoriteColor from './components/lifeCycle/favoriteColor';
-import AnimatedBoxes from './components/boxAnimation/boxes';
-import ChangingBox from './components/changingBox/changingBox';
-import Timer from './components/spinner/timer';
+import Hello from './components/ex1.1-hello/hello'
+import Display from './components/ex2.1-basic/Basic';
+import Boxes from './components/ex3.1-boxes/box1';
+import Quiz from './components/ex3.2-quiz/quiz';
+import Buttons from './components/ex4.1-buttons/buttons';
+import Cards from './components/ex4.2-cards/cards';
+import ClassCards from './components/ex6.1-class_cards/cards';
+import Increment from './components/ex7.1-increment/increment';
+import HideAndSeek from './components/ex7.2-hide_and_seek/hideAndseek';
+import FavoriteColor from './components/ex8.1-life_cycle/favoriteColor';
+import AnimatedBoxes from './components/ex8.2-box_animation/boxes';
+import ChangingBox from './components/ex8.3-changing_box/changingBox';
+import Timer from './components/ex9.1-spinner/timer';
 
 import './App.css';
 
