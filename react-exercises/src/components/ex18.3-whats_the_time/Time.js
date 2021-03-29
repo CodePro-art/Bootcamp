482d04a9e8727d44863ea2fb621f945588d41513
Netanel Mazuz <netazuz@gmail.com>
2021-03-29 23:49:31 +0300

    update

--- react-exercises/src/components/ex18.3-whats_the_time/Time.js
+++ react-exercises/src/components/ex18.3-whats_the_time/Time.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react';
 import TimeItem from './TimeItem'
 import './Time.css'
 
 export default function Time() {
-
+  
+  const [time,setTime] = useState('');
   const types = ["seconds","minutes","hours"];
-  const renderTime = arr => arr.map(t => <TimeItem type={t} />);
+  const mult = [60,1,1/60];
+  const mult2 = [1/60,1,60];
+
+  const updateTime = result => {
+    console.log(result);
+    setTime(result);
+  };
+
+  const renderTime = (arr,time) => 
+    arr.map((e,i) => 
+      <TimeItem 
+        key={i.toString()}
+        type={e} 
+        time={time*mult[i]}
+        multiplyer={mult2[i]}
+        sendTime={updateTime}
+      />
+    );
 
   return (
     <div className="time-container">
       <div className="box1">
         <h1 className="time-title">Time Coverter:</h1>
-        {renderTime(types)}
+        {renderTime(types,time)}
       </div>
     </div>
   )
--- react-exercises/src/components/ex18.3-whats_the_time/TimeItem.js
+++ react-exercises/src/components/ex18.3-whats_the_time/TimeItem.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 export default function TimeItem(props) {
 
+  const re = /^[0-9\b]+$/;
+  const [input,setInput] = useState(props.time || '');
 
+  const callback= (time) => {
+    if (time === '' || re.test(time)){
+      setInput(time)
+      props.sendTime(time*props.multiplyer)
+    }
+  }
+  
   return (
     <div className="time-item">
-      <label className="time-label" htmlFor={props.type}>{props.type.toUpperCase()}:</label>
-      <input type="text" className="time-input" id={props.type}/>
+      <label className="time-label" htmlFor={props.type}> 
+        {props.type.toUpperCase()}: 
+      </label>
+      
+      <input 
+        type="text" 
+        className="time-input" 
+        id={props.type}
+        value={input}
+        autoComplete="off"
+        onChange={(e) => callback(e.target.value) }
+      />
     </div>
   )
 }
